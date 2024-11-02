42e75acbb62bcc0bf70041bda6bd8178b5aa0a0e
Akhilesh TP <panakhi@gmail.com>
2024-11-02 23:48:53 +0530

    second

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://warehouseqc.netlify.app/',
     credentials: true
 }));
 app.use("/api/role", roleRoute);
