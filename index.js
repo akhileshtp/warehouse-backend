931d105417c50cd90234d0ab1e7ba152c750ba7e
akhileshtp <panakhi@gmail.com>
2024-11-03 07:33:01 +0530

    cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://warehouseqc.netlify.app/login/',
+    origin: 'https://warehouseqc.netlify.app/',
     credentials: true
 }));
 app.use("/api/role", roleRoute);
